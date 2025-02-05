fc9e890131fb0cebcac8615648fc0abfa0ef8b03
Nayara <nayarasoares.cat26@gmail.com>
2025-02-05 10:29:33 -0300

    aula 05/02

--- src/main/java/com/senai/Exercícios/CadastroProduto/Main.java
+++ src/main/java/com/senai/Exercícios/CadastroProduto/Main.java
@@ -1,5 +1,31 @@
 package com.senai.Exercícios.CadastroProduto;
 
 public class Main {
+    public static void main(String[] args) {
+        Produto produto1 = new Produto(
+                "Arroz",
+                25.0,
+                100
+        );
+        Produto produto2 = new Produto(
+                "Macarrão",
+                3.0,
+                250
+
+        );
+        Produto produto3 = new Produto(
+                "Feijão",
+                7.0,
+                150
+        );
+
+        System.out.println("Digite a quantidade atualizada do estoque");
+    
+
+        produto1.exibirDetalhes();
+        produto2.exibirDetalhes();
+        produto3.exibirDetalhes();
+
+    }
 
 }
--- src/main/java/com/senai/Exercícios/CadastroProduto/Produto.java
+++ src/main/java/com/senai/Exercícios/CadastroProduto/Produto.java
@@ -1,4 +1,23 @@
 package com.senai.Exercícios.CadastroProduto;
 
+public class Produto {
+    String nome;
+    double preço;
+    int quantidadeInicial;
+
+    public Produto (String nome, double preço, int quantidade) {
+        this.nome = nome;
+        this.preço = preço;
+        this.quantidadeInicial = quantidade;
+    }
+
+    public void exibirDetalhes(){
+        System.out.println("Nome: " + this.nome + "\nPreço:" + this.preço + "\nQuantidade: " + this.quantidadeInicial + "\n");
+
+    }
+
+    public void atualizaEstoque() {
 
 
+    }
+}
