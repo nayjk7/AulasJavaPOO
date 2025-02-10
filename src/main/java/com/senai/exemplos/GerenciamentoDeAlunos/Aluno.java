34889a52ec32798c358ec4e56117919b71fba342
Nayara <nayarasoares.cat26@gmail.com>
2025-02-10 10:52:55 -0300

    exercícios 10/02

---
+++ src/main/java/com/senai/exemplos/GerenciamentoDeAlunos/Aluno.java
@@ -0,0 +1,5 @@
+package com.senai.exemplos.GerenciamentoDeAlunos;
+
+public class Aluno {
+
+}
---
+++ src/main/java/com/senai/exemplos/GerenciamentoDeAlunos/Main.java
@@ -0,0 +1,7 @@
+package com.senai.exemplos.GerenciamentoDeAlunos;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
